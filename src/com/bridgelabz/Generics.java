90905c4b0640798e8b41c3accd22f687060c6d7e
saps999 <saptarshibiswasgang@gmail.com>
2022-09-11 16:11:32 +0530

    Finding maximum float.

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -2,8 +2,8 @@ package com.bridgelabz;
 
 class FindMaximum {
 
-    public void findMaximum(Integer a, Integer b, Integer c) {
-        Integer max = a;
+    public void findMaximum(Float a, Float b, Float c) {
+        Float max = a;
         if (b.compareTo(a) > 0) {
             max = b;
         }
@@ -17,6 +17,6 @@ public class Generics {
     public static void main(String[] args) {
         System.out.println("Welcome to the Day-14 Generics Assignment");
         FindMaximum fm =new FindMaximum();
-        fm.findMaximum(10,20,30);
+        fm.findMaximum(10.3f,20.5f,30.6f);
     }
 }
