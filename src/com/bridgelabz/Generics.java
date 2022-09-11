0a22307813795c02a3777098ea47dbe25189383e
saps999 <saptarshibiswasgang@gmail.com>
2022-09-11 16:08:02 +0530

    Finding Maximum Integer.

--- src/com/bridgelabz/Generics.java
+++ src/com/bridgelabz/Generics.java
@@ -1,7 +1,22 @@
 package com.bridgelabz;
 
+class FindMaximum {
+
+    public void findMaximum(Integer a, Integer b, Integer c) {
+        Integer max = a;
+        if (b.compareTo(a) > 0) {
+            max = b;
+        }
+        if (c.compareTo(max) > 0) {
+            max = c;
+        }
+        System.out.println("Maximum number is: " + max);
+    }
+}
 public class Generics {
     public static void main(String[] args) {
         System.out.println("Welcome to the Day-14 Generics Assignment");
+        FindMaximum fm =new FindMaximum();
+        fm.findMaximum(10,20,30);
     }
 }
